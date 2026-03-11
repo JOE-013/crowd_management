f60255279eee8395c5a2456177d2a9558efbcedf
Joe Anil <aniljoe646@gmail.com>
2026-03-11 20:00:11 +0530

    back

--- yolo/checkYolo.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-try:
-    model = YOLO('models/yolov8s.pt')  # Replace with your path
-    print("YOLOv8 loaded successfully! Model details:")
-    print(model)  # Prints model info
-except Exception as e:
-    print(f"Error loading YOLOv8: {e}")
